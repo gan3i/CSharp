f766c3b549e1164190c6123befcef5474d9f28dc
gan3i <gani468@gmail.com>
2021-01-23 21:13:44 +0530

    MVC app to authenticate using Azure AD

---
+++ ADConnectApp/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly: OwinStartup(typeof(ADConnectApp.Startup))]
+
+namespace ADConnectApp
+{
+    public class Startup
+    {
+        string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+        string redirectUil = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
+        string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenent"];
+        string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
+        private string redirectUri;
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                // Sets the ClientId, authority, RedirectUri as obtained from web.config
+                ClientId = clientId,
+                    Authority = authority,
+                    RedirectUri = redirectUri,
+                // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+                PostLogoutRedirectUri = redirectUri,
+                    Scope = OpenIdConnectScope.OpenIdProfile,
+                // ResponseType is set to request the id_token - which contains basic information about the signed-in user
+                ResponseType = OpenIdConnectResponseType.IdToken,
+                // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
+                TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false // This is a simplification
+                },
+                // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = OnAuthenticationFailed
+                    }
+                }
+            );
+        }
+
+        /// <summary>
+        /// Handle failed authentication requests by redirecting the user to the home page with an error in the query string
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/?errormessage=" + context.Exception.Message);
+            return Task.FromResult(0);
+        }
+    }
+}
