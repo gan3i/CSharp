55d8a9a8aa879d57cce0a0a35cf041a4261c7d9e
gan3i <gani468@gmail.com>
2021-02-03 15:37:34 +0530

    open closed principle

---
+++ ADConnectApp/ReceiveAndSendNotification/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using System;
+
+
+namespace ReceiveAndSendNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+             CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            // Create the subscription that receives messages
+            CreateSubscription(connectionString);
+
+            // Receive message
+            ReceiveMessageAndSendNotification(connectionString);
+        }
+        static void CreateSubscription(string connectionString)
+        {
+            // Create the subscription if it does not exist already
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.SubscriptionExists(sampleTopic, sampleSubscription))
+            {
+                namespaceManager.CreateSubscription(sampleTopic, sampleSubscription);
+
+            }
+        }
+        static void ReceiveMessageAndSendNotification(string connectionString)
+        {
+            // Initialize the Notification Hub
+            string hubConnectionString = CloudConfigurationManager.GetSetting
+                    ("Microsoft.NotificationHub.ConnectionString");
+            hub = NotificationHubClient.CreateClientFromConnectionString
+                    (hubConnectionString, "enterprisepushservicehub");
+
+            SubscriptionClient Client =
+                SubscriptionClient.CreateFromConnectionString
+                        (connectionString, sampleTopic, sampleSubscription);
+
+            Client.Receive();
+
+            // Continuously process messages received from the subscription
+            while (true)
+            {
+                BrokeredMessage message = Client.Receive();
+                var toastMessage = @"<toast><visual><binding template=""ToastText01""><text id=""1"">{messagepayload}</text></binding></visual></toast>";
+
+                if (message != null)
+                {
+                    try
+                    {
+                        Console.WriteLine(message.MessageId);
+                        Console.WriteLine(message.SequenceNumber);
+                        string messageBody = message.GetBody<string>();
+                        Console.WriteLine("Body: " + messageBody + "\n");
+
+                        toastMessage = toastMessage.Replace("{messagepayload}", messageBody);
+                        SendNotificationAsync(toastMessage);
+
+                        // Remove message from subscription
+                        message.Complete();
+                    }
+                    catch (Exception)
+                    {
+                        // Indicate a problem, unlock message in subscription
+                        message.Abandon();
+                    }
+                }
+            }
+        }
+        static async void SendNotificationAsync(string message)
+        {
+            await hub.SendWindowsNativeNotificationAsync(message);
+        }
+    }
---
+++ CSharpPractice/DesignPatterns/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        public enum Color
+        {
+            Red, Green, Blue
+        }
+        public enum Size
+        {
+            Small, Medium, Large, Yuge
+        }
+
+        public class Product
+        {
+            public string Name;
+            public Color Color;
+            public Size Size;
+
+            public Product(string name, Color color, Size size)
+            {
+                if (name == null)
+                {
+                    throw new ArgumentNullException(paramName: nameof(name));
+                }
+                this.Name = name;
+                this.Size = size;
+                this.Color = color;
+            }
+        }
+
+        public interface ISpecification<T>
+        {
+            bool IsSatisfied(T t);
+        }
+
+        public interface IFilter<T>
+        {
+            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+        public class ColorSpecification : ISpecification<Product>
+        {
+            private Color color;
+
+            public ColorSpecification(Color color)
+            {
+                this.color = color;
+            }
+
+            public bool IsSatisfied(Product t)
+            {
+                return t.Color == color;
+            }
+        }
+
+        public class SizeSpecification : ISpecification<Product>
+        {
+            private Size Size;
+            public SizeSpecification(Size size)
+            {
+                this.Size = size;
+            }
+            public bool IsSatisfied(Product product)
+            {
+                return product.Size == this.Size;
+            }
+        }
+
+        public class AndSpecification<T> : ISpecification<T>
+        {
+            private ISpecification<T> first, second;
+
+            public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+            {
+                this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+                this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+            }
+
+            public bool IsSatisfied(T product)
+            {
+                return first.IsSatisfied(product) && second.IsSatisfied(product);
+            }
+
+        }
+
+        public class BetterFilter : IFilter<Product>
+        {
+            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+            {
+                foreach (var p in items)
+                {
+                    if (spec.IsSatisfied(p))
+                    {
+                        yield return p;
+                    }
+                }
+            }
+        }
+
+
+        public class ProductFilter
+        {
+            public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+            {
+                foreach (var p in products)
+                {
+                    if (p.Size == size)
+                    {
+                        yield return p;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[]
+             {
+                new Product("Apple", Color.Green, Size.Small),
+                new Product("Tree", Color.Green, Size.Large),
+                new Product("Tree", Color.Blue, Size.Large)
+             };
+            var bf = new BetterFilter();
+            foreach (var p in bf.Filter(products,
+                new AndSpecification<Product>(new ColorSpecification(Color.Green), new SizeSpecification(Size.Large))))
+            {
+                Console.WriteLine($"name : {p.Name}");
+            }
+            foreach (var p in bf.Filter(products,new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($"name : {p.Name}");
+            }
+        }
+    }
+}
